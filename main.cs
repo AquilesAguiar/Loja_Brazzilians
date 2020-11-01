cfaed367438f2085db5117978a0c557eb7288682
Aquiles Aguiar <aquilessilva@ucl.br>
2020-11-01 15:03:12 +0000

    main

--- main.cs
+++ main.cs
@@ -28,9 +28,9 @@ class MainClass {
 /*-------------------------------------------------------*///Funcionamento da Loja
 /*-------------------------------------------------------*/   
     //Definindo Variaveis
-    string NomeC,endereco,email,sexo,decis = "S",deleta;
-    double cpf,tele,idade,total = 0;
-    int codigo, quantRet,quantTo,quantCar;
+    string NomeC,endereco,email,sexo,deleta,pagar;
+    double cpf,tele,idade,total = 0,decis = 0;
+    int codigo, quantRet;
 
     //mensagem de boas vindas
 
@@ -76,154 +76,141 @@ Console.Clear();
 
     Console.Clear();//Limpa Terminal
 
-    //Criando Carrinho_Compras
 
-    //Mostrando Itens da Loja
-    Console.WriteLine(@"             Lista De Produtos Brazillians ");
-    for(int x = 0; x < produtos.get_NomeProdutos().Count; x++){
+   //Funcionamento Da loja
+    while(decis != 5){
+
+      Console.Write("Escolha o que deseja Fazer\n 1 - Mostrar Catálogo de Produtos\n 2 - Comprar Itens\n 3 - Mostrar Carrinho de Compras\n 4 - Retirar Itens do Carrinho\n 5 - Finalizar Compra\n >>");
+
+      decis = int.Parse(Console.ReadLine());
+
+      Console.Clear();
+
+      //Mostra Catálogo
+      if(decis == 1){
+        //Mostrando Itens da Loja
+        Console.WriteLine(@"             Lista De Produtos Brazillians ");
+        for(int x = 0; x < produtos.get_NomeProdutos().Count; x++){
         Console.WriteLine($@"
-Código >> {produtos.get_CodProdutos()[x]}   |   Produto >> {produtos.get_NomeProdutos()[x]}  |   Preço >>  {produtos.get_PrecoProdutos()[x]} | ");}
-
-    //adiciona itens ao carrinho de Compra
-    while(decis == "S"){
-      
-      //Passando o código do produto a ser escolido
-      Console.Write("\n\n\nInsira o Código do Produto que deseja adquirir >>");
-      codigo = int.Parse(Console.ReadLine());
-
-      
-      //Quantidade que será retirada do estoque
-      Console.Write("Insira a quantidade do Produto que deseja adquirir >>");
-      quantRet = int.Parse(Console.ReadLine());
-      
-      // Vai rodar até achar o índice certo.
-      for(int x = 0; x < produtos.get_QtdProdutos().Count; x++) {
-       
-        // Se o código for igual ao código do produto
-        if (codigo == produtos.get_CodProdutos()[x]){
-          
-          // Se estoque estiver disponivel adiciona na lista
-          if(quantRet <= produtos.get_QtdProdutos()[x] ){
-            
-            //Retirando a quantidade solicitada do Estoque
-            //[RN - 02 retirar os itens do estoque]
-            produtos.get_QtdProdutos()[x] = produtos.get_QtdProdutos()[x] - quantRet; 
-
-            
-            //Adicionando itens no Construtor Carrinho
-            NovoCarrinho = new carrinho_compra(produtos.get_NomeProdutos()[x],quantRet,produtos.get_PrecoProdutos()[x],produtos.get_CodProdutos()[x]);
-
-            
-            // Adicionando na Lista carrinho
-            ListaCarrinho.Add(NovoCarrinho);
-            Console.WriteLine("Itens adicionado no Carrinho");
-           }
-          
-          // Se não printa uma mensagem de erro [RN - 01 Cliente não pode escolher quantidade de produto abaixo do estoque]
-          else{
-            Console.WriteLine("Quantidade de Estoque abaixo do esperado");
-          }
+Código >> {produtos.get_CodProdutos()[x]}   |   Produto >> {produtos.get_NomeProdutos()[x]}  |   Preço >>  {produtos.get_PrecoProdutos()[x]} | ");
         }
       }
-      //Sair do Cadastro de itens na lista
-      Console.Write("CONTINUAR? S/N >>");
-      decis = Console.ReadLine();
-    }
-
-    //Zerando a Variavel
-    decis = "S";
-
-    //Limpando o Terminal
-    Console.Clear();
-
-   //Modificações e Visualização do Carrinho de Compras
-    do{
-      total = 0;
       
-      //Printa o Carrinho para o cliente
-      Console.WriteLine($@"                       Carrinho de Compra");
-      for(int x = 0; x < ListaCarrinho.Count;x++ ){
-         Console.WriteLine($@"                       
-----------------------------------------------------------
+      // Compra Itens
+      if(decis == 2){
 
-Código >> {ListaCarrinho[x].get_cod()}
-Produto >> {ListaCarrinho[x].get_produto()}
-Valor Por Quantidade >> R${ListaCarrinho[x].get_valor()} X {ListaCarrinho[x].get_quantidade()} = R${NovoCarrinho.calcula_valor(ListaCarrinho[x].get_valor(),ListaCarrinho[x].get_quantidade())}
-");   
-      //Realiza o Caculo para o Total da compra realizada
-      total = total+NovoCarrinho.calcula_valor(ListaCarrinho[x].get_valor(),ListaCarrinho[x].get_quantidade());
-      }
-    Console.WriteLine($@"
-Total da Compra >> R${total}");
+        //Codigo do Produto
+        Console.Write("Insira o Código do Produto que deseja adquirir >>");
+        codigo = int.Parse(Console.ReadLine());
+
       
-      //Decisão Para retirar item do Carrinho
-      Console.Write("\n\nDeseja Retirar algum item do carrinho S/N >>");
-      deleta = Console.ReadLine();
-
-      //Caso deseja apagar algum item
-      if(deleta == "S"){
-      
-       //Deve se passa o código do produto que deseja apagar
-       Console.Write("\n\n\nInsira a posição do item  no carrinho sendo 1 o primeiro item >>");
-       codigo = int.Parse(Console.ReadLine());
-
-        //Um for irá percorrer a Lista
-        for(int x = 0; x < ListaCarrinho.Count;x++ ){
-          
-          //Quando achar o código do produto
-          if(codigo  == ListaCarrinho[x].get_cod() ){
-
-              //Remove em todos os itens daquele indice da lista
-
-              Console.WriteLine("Apagando....");
-              ListaCarrinho.RemoveAt(x);
+        //Quantidade que será retirada do estoque
+        Console.Write("Insira a quantidade do Produto que deseja adquirir >>");
+        quantRet = int.Parse(Console.ReadLine());
+        
+        // Vai rodar até achar o índice certo.
+        for(int x = 0; x < produtos.get_QtdProdutos().Count; x++) {
+        
+          // Se o código for igual ao código do produto
+          if (codigo == produtos.get_CodProdutos()[x]){
+            
+            // Se estoque estiver disponivel adiciona na lista
+            if(quantRet <= produtos.get_QtdProdutos()[x] ){
               
-              //[RN - 02 adiciona Novamente os itens ao estoque]
-              produtos.get_QtdProdutos()[x] = produtos.get_QtdProdutos()[x] + ListaCarrinho[x].get_quantidade();
+              //Adicionando itens no Construtor Carrinho
+              NovoCarrinho = new carrinho_compra(produtos.get_NomeProdutos()[x],quantRet,produtos.get_PrecoProdutos()[x],produtos.get_CodProdutos()[x]);
+
+              // Adicionando na Lista carrinho
+              ListaCarrinho.Add(NovoCarrinho);
+              Console.WriteLine("Itens adicionado no Carrinho");
+            }
+            
+            // Se não printa uma mensagem de erro [RN - 01 Cliente não pode escolher quantidade de produto abaixo do estoque]
+            else{
+              Console.WriteLine("Quantidade de Estoque abaixo do esperado");
+            }
           }
         }
       }
-      //Senão o while se encerra
-      else{
-        decis = "N";
-     }
-    
 
-    }
-    while(decis == "S");
+      //Mostra Carrinho
+      if(decis == 3){
+        total = 0;
+      
+        //Printa o Carrinho para o cliente
+        Console.WriteLine($@"                       Carrinho de Compra");
+        for(int x = 0; x < ListaCarrinho.Count;x++ ){
+          Console.WriteLine($@"                       
+  ----------------------------------------------------------
 
-    //Limpando o Terminal
-    Console.Clear();
-    
-    Console.WriteLine("Carrinho De Compra");
-    for(int x = 0; x < ListaCarrinho.Count;x++ ){
-         Console.WriteLine($@"                       
-----------------------------------------------------------
+  Código >> {ListaCarrinho[x].get_cod()}
+  Produto >> {ListaCarrinho[x].get_produto()}
+  Valor Por Quantidade >> R${ListaCarrinho[x].get_valor()} X {ListaCarrinho[x].get_quantidade()} = R${NovoCarrinho.calcula_valor(ListaCarrinho[x].get_valor(),ListaCarrinho[x].get_quantidade())}
+  ");   
+        //Realiza o Caculo para o Total da compra realizada
+        total = total+NovoCarrinho.calcula_valor(ListaCarrinho[x].get_valor(),ListaCarrinho[x].get_quantidade());
+        }
+        Console.WriteLine($"\nTotal da Compra >> R${total}");       
+      }
 
-Código >> {ListaCarrinho[x].get_cod()}
-Produto >> {ListaCarrinho[x].get_produto()}
-Valor Por Quantidade >> R${ListaCarrinho[x].get_valor()} X {ListaCarrinho[x].get_quantidade()} = R${NovoCarrinho.calcula_valor(ListaCarrinho[x].get_valor(),ListaCarrinho[x].get_quantidade())}
-");    
-    }
-      //[RN - 03 Simulação de pagamento]
-      //Simulação de pagamento
-       Console.WriteLine($"Total da Compra >> R${total}");
-      while(true){
-        Console.Write("Como deseja pagar ? Boleto/cartao >>");
-       decis = Console.ReadLine();
+      //Retira Itens do Carrinho
+      if(decis == 4){
+
+         //Deve se passa o código do produto que deseja apagar
+        Console.Write("Insira o código que deseja apagar >>");
+        codigo = int.Parse(Console.ReadLine());
+
+          //Um for irá percorrer a Lista
+          for(int x = 0; x < ListaCarrinho.Count;x++ ){
+            
+            //Quando achar o código do produto
+            if(codigo  == ListaCarrinho[x].get_cod() ){
+
+                //Remove em todos os itens daquele indice da lista
+
+                Console.WriteLine("Apagando....");
+                ListaCarrinho.RemoveAt(x);
+                
+                //[RN - 02 Remove os itens do estoque]
+                produtos.get_QtdProdutos()[x] = produtos.get_QtdProdutos()[x] - ListaCarrinho[x].get_quantidade();
+            }
+          }
+      }
+
+      //Finaliza Compra
+      if(decis == 5){
+
+        Console.WriteLine("Carrinho De Compra");
+
+        for(int x = 0; x < ListaCarrinho.Count;x++ ){
+            Console.WriteLine($@"                       
+    ----------------------------------------------------------
+
+    Código >> {ListaCarrinho[x].get_cod()}
+    Produto >> {ListaCarrinho[x].get_produto()}
+    Valor Por Quantidade >> R${ListaCarrinho[x].get_valor()} X {ListaCarrinho[x].get_quantidade()} = R${NovoCarrinho.calcula_valor(ListaCarrinho[x].get_valor(),ListaCarrinho[x].get_quantidade())}
+    ");    
+        }
+          //[RN - 03 Simulação de pagamento]
+          //Simulação de pagamento
+        Console.WriteLine($"Total da Compra >> R${total}");
+        
+        Console.Write("\nComo deseja pagar ? Boleto/cartao >>");
+        pagar = Console.ReadLine();
+          
         cpf = ListaClientes[0].get_cpf();
 
-        if (decis.ToLower() == "boleto"){
-         Console.WriteLine(pagamento.PagarBoleto(total,cpf));
-         break;
-       }
-        else if (decis.ToLower() == "cartao"){
+        if (pagar.ToLower() == "boleto"){
+          Console.WriteLine(pagamento.PagarBoleto(total,cpf));
+        }
+            
+        else if (pagar.ToLower() == "cartao"){
           Console.Write("Insira o valor do seu cartão >>");
           double valorcart =  double.Parse(Console.ReadLine());
 
           Console.WriteLine(pagamento.PagarCartao(total,valorcart,cpf));
-          break;
-        }
+          }
       }
+    } 
   }
 }
